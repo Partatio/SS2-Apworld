a20f03c4f07698952567c41075849f31b5d5da05
BuildTools <unconfigured@null.spigotmc.org>
2025-03-26 09:32:03 -0700

    changed path option wording

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import settings
 
 class SS2Settings(settings.Group):
     class SS2folder(settings.UserFolderPath):
-        """Folder path of your System Shock 2 installation"""
+        """Path to your System Shock 2 installation root directory"""
         description = "System Shock 2 installation folder"
     
     ss2_path: SS2folder = SS2folder("C:/Program Files (x86)/Steam/steamapps/common/SS2")
