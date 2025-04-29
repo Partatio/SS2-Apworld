8a941dec1fca290d6eec6e85980cab5561d52c19
BuildTools <unconfigured@null.spigotmc.org>
2025-04-29 06:31:14 -0700

    Changed launch for 0.6 update

--- __init__.py
+++ __init__.py
@@ -6,13 +6,13 @@ from .settings import SS2Settings
 from worlds.AutoWorld import World
 from worlds.generic.Rules import add_rule, forbid_item
 from BaseClasses import Region, Location, Entrance, Item, ItemClassification, CollectionState
-from worlds.LauncherComponents import components, Component, Type, launch_subprocess
+from worlds.LauncherComponents import components, Component, Type, launch as launch_component
 
 from Utils import visualize_regions
 
 def launch_client(*args):
     from .Client import launch
-    launch_subprocess(launch, "SS2Client", args) #todo when 6.0 update: change launch_subprocess import to "launch as launch_component" and change launch_subprocess in this line to launch_component.
+    launch_component(launch, "SS2Client", args)
 
 components.append(Component("System Shock 2 Client", func=launch_client, component_type=Type.CLIENT))
 
